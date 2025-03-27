569f2de89c14f444f6ec31f459072b64a1098ae4
Afnan Zaman Mohammad Sajid <sajidzaman.2002.3@gmail.com>
2025-03-27 22:34:27 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ btn.addEventListener('click', () => {
   nav.classList.toggle('flex')
   nav.classList.toggle('hidden')
 })
+
+document.addEventListener('click', (event) => {
+  if(nav.classList.contains('flex') && !event.target.closest('#menu') && !event.target.closest('#menu-btn')) {
+    nav.classList.toggle('flex')
+    nav.classList.toggle('hidden')
+  }
+})
